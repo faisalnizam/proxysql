a739d222a333bbe7150715c928c5a085f6dda07c
Faisal Nizam <faisal@logiik.com>
2022-10-09 18:35:47 +0400

    Update state.tf

--- state.tf
+++ state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "eyewacore-master-ap-southeast-1-tfstates"
-    key     = "ou/application/eyewa-uat/terraform.tfstate"
+    key     = "ou/application/uat/terraform.tfstate"
     region  = "ap-southeast-1"
     profile = "eyewa_s3"
   }
